870ab3138ebce561a82f3fb63b58dc750aea437e
savad-k <savadmohmd808@gmail.com>
2023-09-29 12:20:37 +0530

    Initial commit

---
+++ bin/datatypes.dart
@@ -0,0 +1,12 @@
+void main(){
+  String name = 'anu';
+  int age = 20;
+  int phone = 7561866766;
+  double cgpa = 8.5;
+  String email = 'savadmohmd@gmail.com';
+  print('my name is $name');
+  print('iam $age yrs old');
+  print('phone no is $phone');
+  print('my cgpa is $cgpa');
+  print('my email id is $email');
+}
---
+++ bin/first_sample.dart
@@ -0,0 +1,5 @@
+void main() {
+  print('my name is savad');
+  print(30);
+  print('my phone number is 07561866766');
+  print(5.5);
